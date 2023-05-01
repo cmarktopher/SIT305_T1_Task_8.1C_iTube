e1dc896e5a954cb0a0d9f05c702e0edb000a93db
chris-marktopher <cmarktopher@gmail.com>
2023-05-01 14:42:00 +1000

    Introduced Navigation Component

--- app/src/main/java/com/application/itube/Activities/MainActivity.java
+++ app/src/main/java/com/application/itube/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.application.itube;
+package com.application.itube.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.application.itube.R;
 
 public class MainActivity extends AppCompatActivity {
 
